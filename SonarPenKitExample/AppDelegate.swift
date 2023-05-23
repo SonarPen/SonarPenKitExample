3bc882ea7f6b8407ec74227dd6169d83981916a4
Water Lou <water@waterworld.com.hk>
2023-05-23 15:16:44 +0800

    init

--- SonarPenKitExample/AppDelegate.swift
+++ SonarPenKitExample/AppDelegate.swift
@@ -1,36 +1,27 @@
-//
-//  AppDelegate.swift
-//  SonarPenKitExample
-//
-//  Created by Water Lou on 22/5/2023.
-//
+/*
+See LICENSE folder for this sample’s licensing information.
+
+Abstract:
+The application delegate maintains the app's life cycle.
+*/
+
+/// PencilKitDraw is a drawing app that demonstrates how to use the PencilKit framework.
+
+/// It shows how you can use PKCanvasView and PKDrawing classes for a great pencil drawing experience,
+/// featuring a thumbnail viewer, a drawing canvas with the system tool picker, and a signature pane popover.
+
+/// While PencilKit is optimized for the pencil drawing experience, it also allows users to draw with a finger.
 
 import UIKit
 
-@main
+@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        application.applicationSupportsShakeToEdit = true
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
 
 }
 
